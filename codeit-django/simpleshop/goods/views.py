11cc1a1831e4c036386743aa0722ba780824e905
YeomJongSeop <bhs3435@naver.com>
2024-02-13 20:47:45 +0900

    Delete codeit-django directory

--- codeit-django/simpleshop/goods/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from goods.models import Product
-
-# Create your views here.
-def index(request):
-    context={}
-    goods = Product.objects.all()
-    context["goods"]=goods
-    return render(request,'goods/index.html',context=context)
